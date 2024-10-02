38f8d53ec1616e5e157864a277e44efc46c16717
HughReadman <166963435+HughReadman@users.noreply.github.com>
2024-10-02 12:42:19 +0800

    Add random function for selecting message components

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 const countries = ["France", "Italy", "Spain", "Belgium", "Australia", "New Zealand", "USA", "Canada", "Thailand", "China", "Vietnam", "Egypt"];
 const travelMethod = ["Train", "Bus", "Car", "Taxi", "Moped", "Skateboard", "Bicycle", "Rollerblades", "Horseback", "Airplane", "Helicopter", "foot"];
 const time = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+const random = (arr) => {
+    return (Math.floor(Math.random() * arr.length))
+}
+
+const randomCountry= random(countries);
+const randomTravel = random(travelMethod);
+const randomTime = random(time);
+
+console.log(countries[randomCountry]);
+console.log(travelMethod[randomTravel]);
+console.log(time[randomTime]);
